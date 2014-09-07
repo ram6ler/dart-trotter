91644d7a5e6292b858fcd26a8062e29fc428932d
Richard Ambler <rambler@ibwya.net>
2014-09-07 22:12:41 +0800

    added Subset class

--- trotter.dart
+++ trotter.dart
@@ -1,28 +1,25 @@
 library trotter;
 
 Map<int, int> _factCache = {};
+Map<int, int> _twoToCache = {};
 
 class _Combinatoric {
   List _elements;
-  int _r, _length;
+  int _length;
   List get elements => _elements;
-  int get r => _r;
   int get length => _length;
     
   static int _fact(int n) => 
       _factCache.containsKey(n) ? _factCache[n] : 
         (n < 2 ? 1 : _factCache[n] = n * _fact(n - 1));
   
+  static int _twoTo(int n) =>
+      _twoToCache.containsKey(n) ? _twoToCache[n] :
+        (n < 1 ? 1 : _twoToCache[n] = 2 * _twoTo(n - 1));
+  
   static int _nPr(int n, int r) => _fact(n) ~/ _fact(n - r);
   
   static int _nCr(int n, int r) => _nPr(n, r) ~/ _fact(r);
- 
-  /**
-   * Returns _k_th permutation in a pseudo-list
-   * "containing" all possible permutations of the objects
-   * in _elements_.
-   * 
-   * */
   
   static List _permWorker(int k, List elements) {
     int n = elements.length;
@@ -39,13 +36,6 @@ class _Combinatoric {
       )..insert(position, elements[n - 1]);
     }
   }
-  
-  /**
-   * Returns _k_th _r_-combination in a pseudo-list
-   * "containing" all possible _r_-combinations of the objects
-   * in _elements_.
-   * 
-   * */
   
   static List _combination(int k, int r, List elements) {
     int
@@ -68,13 +58,6 @@ class _Combinatoric {
     }
   }
   
-  /**
-     * Returns _k_th _r_-permutation in a pseudo-list
-     * "containing" all possible _r_-permutations of the objects
-     * in _elements_.
-     * 
-     * */
-  
   static List _permutation(int k, int r, List elements) {
     int
       n = elements.length,
@@ -86,7 +69,13 @@ class _Combinatoric {
     return _permWorker(item, comb);
   }
   
-  
+  static List _subset(int k, List elements) {
+    k = _adjustedIndex(k, _twoTo(elements.length));
+    List r = [];
+    for (int i = 0; i < elements.length; i++)
+      if (k & _twoTo(i) != 0) r.add(elements[i]);
+    return r;
+  }
   
   static int _adjustedIndex(int k, int n) {
     while (k < 0) k += n;
@@ -96,6 +85,9 @@ class _Combinatoric {
 }
 
 class Permutations extends _Combinatoric {
+  int _r;
+  int get r => _r;
+
   Permutations(int r, List elements) {
     assert(r >= 0 && r <= elements.length);
     _elements = new List.from(elements);
@@ -103,7 +95,6 @@ class Permutations extends _Combinatoric {
     _length = _Combinatoric._nPr(elements.length, r);
   }
   
-  // returns the kth permutation
   List operator [](int k) => _Combinatoric._permutation(
     _Combinatoric._adjustedIndex(k, length), 
     r, 
@@ -112,6 +103,9 @@ class Permutations extends _Combinatoric {
 }
 
 class Combinations extends _Combinatoric {
+  int _r;
+  int get r => _r;
+  
   Combinations(int r, List elements) {
     assert(r >= 0 && r <= elements.length);
     _elements = new List.from(elements);
@@ -119,10 +113,21 @@ class Combinations extends _Combinatoric {
     _length = _Combinatoric._nCr(elements.length, r);
   }
     
-  // returns the kth combination
   List operator [](int k) => _Combinatoric._combination(
     _Combinatoric._adjustedIndex(k, length), 
     r, 
     elements
   );
 }
+
+class Subsets extends _Combinatoric {
+
+  Subsets(List elements) {
+    _elements = new List.from(elements);
+    _length = _Combinatoric._twoTo(elements.length);
+  }
+  
+  List operator [](int k) => _Combinatoric._subset(
+    _Combinatoric._adjustedIndex(k, length), elements);
+}
+
